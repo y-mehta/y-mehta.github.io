3408b83f70e1d001cbb168842790f93ce34db009
Yash Mehta <24428063+y-mehta@users.noreply.github.com>
2024-06-14 23:41:20 +0530

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -2,14 +2,14 @@
 
 const CONFIG = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (This is the only required config)
+    username: 'y-mehta', // Your GitHub org/user name. (This is the only required config)
   },
   /**
    * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
    * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
    * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
    */
-  base: '/gitprofile/',
+  base: '/',
   projects: {
     github: {
       display: true, // Display GitHub projects?
@@ -19,48 +19,33 @@ const CONFIG = {
         sortBy: 'stars', // Sort projects by 'stars' or 'updated'
         limit: 8, // How many projects to display.
         exclude: {
-          forks: false, // Forked projects will not be displayed if set to true.
+          forks: true, // Forked projects will not be displayed if set to true.
           projects: [], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
         },
       },
       manual: {
         // Properties for manually specifying projects
-        projects: ['arifszn/gitprofile', 'arifszn/pandora'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: ['y-mehta/ssrf-req-filter', 'y-mehta/vulnalerts'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
     external: {
       header: 'My Projects',
       // To hide the `External Projects` section, keep it empty.
       projects: [
-        {
-          title: 'Project Name',
-          description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-          imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
-        },
-        {
-          title: 'Project Name',
-          description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-          imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
-        },
+       
       ],
     },
   },
   seo: {
-    title: 'Portfolio of Ariful Alam',
-    description: '',
+    title: 'Yash Mehta',
+    description: 'A Security Enthusiast with a deep interest in AppSec, Automation and Software Architecture.',
     imageURL: '',
   },
   social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
-    mastodon: 'arifszn@mastodon.social',
-    researchGate: '',
+    linkedin: '1yashm',
+    twitter: 'YashCMehta',
+    mastodon: '',
+    researchGate: 'Yash-Mehta-26',
     facebook: '',
     instagram: '',
     reddit: '',
@@ -69,97 +54,130 @@ const CONFIG = {
     udemy: '',
     dribbble: '',
     behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
+    medium: '',
+    dev: 'ymehta',
     stackoverflow: '', // example: '1/jeff-atwood'
     skype: '',
     telegram: '',
-    website: 'https://www.arifszn.com',
+    website: '',
     phone: '',
-    email: 'arifulalamszn@gmail.com',
+    email: 'yash.mehta@owasp.org',
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      '', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
-    'PHP',
-    'Laravel',
+    'Application Security',
+    'Cloud Security',
     'JavaScript',
-    'React.js',
+    'AWS',
     'Node.js',
-    'Nest.js',
-    'MySQL',
-    'PostgreSQL',
-    'Git',
-    'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
+    'Kubernetes',
+    'Penetration Testing',
+    'Security Automation',
+    'Terraform'
   ],
   experiences: [
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
+      company: 'Postman',
+      position: 'Senior Security Engineer',
+      from: 'April 2024',
       to: 'Present',
-      companyLink: 'https://example.com',
+      companyLink: 'https://postman.com',
     },
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
+      company: 'Postman',
+      position: 'Security Engineer II',
+      from: 'May 2022',
+      to: 'April 2024',
+      companyLink: 'https://postman.com',
+    },
+    {
+      company: 'Postman',
+      position: 'Security Engineer',
+      from: 'October 2020',
+      to: 'May 2022',
+      companyLink: 'https://postman.com',
+    },
+    {
+      company: 'Postman',
+      position: 'Security Engineer, Intern',
+      from: 'April 2020',
+      to: 'October 2020',
+      companyLink: 'https://postman.com',
+    },
+    {
+      company: 'Astra Security',
+      position: 'Web Security Intern',
+      from: 'May 2019',
+      to: 'July 2019',
+      companyLink: 'https://getastra.com',
+    },
+    {
+      company: 'Astra Security',
+      position: 'Web Security Intern',
+      from: 'May 2018',
+      to: 'July 2018',
+      companyLink: 'https://getastra.com',
+    },
+    {
+      company: 'Cyber Crime Cell, Gurugram Police',
+      position: 'Security Intern',
+      from: 'June 2017',
+      to: 'June 2017',
+      companyLink: '',
+    },
+    {
+      company: 'FreeCopy',
+      position: 'IT Consultant',
+      from: '2017',
+      to: '2018',
+      companyLink: '',
     },
   ],
   certifications: [
     {
-      name: 'Lorem ipsum',
-      body: 'Lorem ipsum dolor sit amet',
-      year: 'March 2022',
-      link: 'https://example.com',
+      name: 'AWS Certified Security – Specialty',
+      body: '',
+      year: 'May 2023',
+      link: 'https://www.credly.com/badges/c17ed6ae-0319-4e71-8f84-0a9164c00d36/linked_in_profile',
+    },
+        {
+      name: 'AWS Certified Solutions Architect - Associate',
+      body: '',
+      year: 'April 2022',
+      link: 'https://www.credly.com/badges/417bb726-4df1-4985-affa-60b8cd36bf67/public_url',
+    },
+            {
+      name: 'GIAC Web Application Penetration Tester (GWAPT)',
+      body: '',
+      year: 'August 2021',
+      link: '',
+    },
+    
+            {
+      name: 'CompTIA Cybersecurity Analyst (CySA+)',
+      body: '',
+      year: 'March 2019',
+      link: '',
     },
   ],
   educations: [
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
-      to: '2019',
-    },
-    {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2012',
-      to: '2014',
-    },
+      institution: 'Ahmedabad University',
+      degree: 'BTech in Information & Communication Technology',
+      from: '2016',
+      to: '2020',
+    }
   ],
   publications: [
-    {
-      title: 'Publication Title',
-      conferenceName: '',
-      journalName: 'Journal Name',
-      authors: 'John Doe, Jane Smith',
-      link: 'https://example.com',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    },
-    {
-      title: 'Publication Title',
-      conferenceName: 'Conference Name',
-      journalName: '',
-      authors: 'John Doe, Jane Smith',
-      link: 'https://example.com',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    },
+    
   ],
   // Display articles from your medium or dev account. (Optional)
   blog: {
     source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
+    username: 'ymehta', // to hide blog section, keep it empty
     limit: 2, // How many articles to display. Max is 10.
   },
   googleAnalytics: {
